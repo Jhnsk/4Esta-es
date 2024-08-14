8063430444c2d18b1f6c67cc50ab74d325eb5112
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2024-08-14 11:01:48 -0300

    Add files via upload

---
+++ vinhJson.js
@@ -0,0 +1,26 @@
+let vinhItens = [
+  {
+    name: 'Pérgola',
+    desc: 'Taça',
+    price: 'R$: 9,90',
+    img: 'pergolaT.jpeg',
+  },
+  {
+    name: 'Pérgola',
+    desc: 'Litro',
+    price: 'R$: 39,90',
+    img: 'pergolaG.jpeg',
+  },
+  {
+    name: 'Chalise',
+    desc: 'Seco ou suave',
+    price: 'R$: 28,90',
+    img: 'chalise.jpeg',
+  },
+  {
+    name: 'Santa Helena',
+    desc: '',
+    price: 'R$: 59,90',
+    img: 'helena.jpeg',
+  },
+  ]
